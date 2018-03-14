b60ab6a7ff831a70bccd0edd86a251785d908c88
Riley Green <rileygreenviiiv@gmail.com>
2018-03-14 14:29:48 -0400

    Add greet service
    Adds root route with greeting to clients

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 300
+
+app.get('/', (req,res)=> res.send("Hello"));
+
+app.listen(PORT, () =>{
+  //eslint-disable-next-line no-console
+  console.log(`This port is ${PORT}`);
+});
