0c2fe3efb7a677cb9b011db53e50026801bdc39b
Riley Green <rileygreenviiiv@gmail.com>
2018-03-14 14:46:18 -0400

    Updated the server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
 const PORT = process.env.PORT || 300;
 
-app.get("/", (req,res)=> res.send("Hello"));
+app.get('/', (req, res) => res.send('Hello'));
 
-app.listen(PORT, () =>{
-	//eslint-disable-next-line no-console
-	console.log(`This port is ${PORT}`);
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`This port is ${PORT}`);
 });
