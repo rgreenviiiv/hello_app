3ded4b090ba7d5e2a023f413b5f866ad7791d6dc
Riley Green <rileygreenviiiv@gmail.com>
2018-03-14 14:36:29 -0400

    Deleted the .eslint file and updated eslint

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 300
+const PORT = process.env.PORT || 300;
 
-app.get('/', (req,res)=> res.send("Hello"));
+app.get("/", (req,res)=> res.send("Hello"));
 
 app.listen(PORT, () =>{
-  //eslint-disable-next-line no-console
-  console.log(`This port is ${PORT}`);
+	//eslint-disable-next-line no-console
+	console.log(`This port is ${PORT}`);
 });
